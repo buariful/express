f27bf4b0a4937e7ccc00393dbbc08dd28ed547d1
buariful <arifulislam321054@gmail.com>
2022-09-15 17:27:21 +0600

    dsjfljs

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
     res.send('server is running smoothly')
 })
 
+app.get('/abc', (req, res) => {
+    res.send('abc')
+})
+app.get('/5', (req, res) => {
+    res.send('5')
+})
+app.get('/e', (req, res) => {
+    res.send('e')
+})
+
 app.listen(port, () => {
     console.log(`app running ${port}`)
 })
