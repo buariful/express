d94faeed05d385742cf483cd5ee26b39fc1ee2c0
buariful <arifulislam321054@gmail.com>
2022-09-15 23:41:03 +0600

    jsflkjslkf

--- index.js
+++ index.js
@@ -3,19 +3,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('server is running smoothly')
 })
 
-app.get('/abc', (req, res) => {
-    res.send('abc')
-})
-app.get('/5', (req, res) => {
-    res.send('5')
-})
-app.get('/e', (req, res) => {
-    res.send('e')
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ki5j5b8.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("dateme").collection("users");;
+
+        app.get('/users', async (req, res) => {
+
+            const query = {};
+            const cursor = database.find(query);
+            const users = await cursor.toArray();
+            res.send(users)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
     console.log(`app running ${port}`)
